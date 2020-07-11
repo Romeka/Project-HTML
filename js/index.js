2d37a9420651eb92a52c23a4321c6040db12cc0e
Romeka <romeka.stabl@gmail.com>
2020-07-11 23:29:35 +0300

    Sample code

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const display = document.querySelector('.basket');
+
+let oneStatus = false;
+
+document.querySelector('.card-button').addEventListener('click',addToBasket)
+  function addToBasket {
+    if(oneStatus==false){
+        oneStatus=true
+        display.value 
+    }
+      }
+      else{ 
+        oneStatus=false;
+        
+      }
+  }
